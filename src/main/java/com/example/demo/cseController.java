ae27238ef43ae48d3b3265e2c981d7987d6ff636
Lavanya Sundarapu <lavanyadevika1122@gmail.com>
2026-02-26 12:11:47 +0530

    second commit

--- src/main/java/com/example/demo/cseController.java
+++ src/main/java/com/example/demo/cseController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class cseController {
-	@GetMapping("/adityaCse")
+	@GetMapping("/home")
 	public String rocks() {
 		return "Welcome to aditya CSE";
 	}
 	
-	@GetMapping("/adityaIt")
+	@GetMapping("/cse")
 	public String rocks1() {
 		return "Welcome ti Aditya IT";
 	}
